934a163c2cf551c950bc071b2c7f1100ced046e1
János Veres <janos@dot.industries>
2024-03-27 11:50:49 +0100

    test: fix integration tests

--- integration/run.sh
+++ integration/run.sh
@@ -3,15 +3,10 @@ set -eu
 
 echo "Starting dev env"
 docker-compose -f ./docker/docker-compose-dev.yml up -d
-echo "Starting moar registry"
-# docker run --name moar --rm --detach --publish 8000:8000 dotindustries/moar-registry:latest
-# S3_ACCESS_KEY_ID=minio S3_SECRET_ACCESS_KEY=minio123 go run cli/main.go -c docker/.dev.yaml up -d > /dev/null 2>&1 &
-S3_ACCESS_KEY_ID=minio S3_SECRET_ACCESS_KEY=minio123 go run cli/main.go up -d > /dev/null 2>&1 &
 
 source integration/wait.sh
 
 source integration/test.sh
 
-echo "Stopping moar instance"
-kill %1
+echo "Stopping dev environment"
 docker-compose -f ./docker/docker-compose-dev.yml down
