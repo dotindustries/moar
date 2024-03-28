c80ba34458c1628cb3842f2fd7ba0e0f9a28de32
nadilas <janos@dot.industries>
2024-03-28 18:03:31 +0100

    Update run.sh
    Signed-off-by: nadilas <janos@dot.industries>

--- integration/run.sh
+++ integration/run.sh
@@ -4,6 +4,8 @@ set -eu
 echo "Starting dev env"
 docker compose -f ./docker/docker-compose-dev.yml up --detach --wait
 
+docker ps -a
+
 source integration/test.sh
 
 echo "Stopping dev environment"
