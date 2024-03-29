e4a00fc03ff1387f632c42abaec7e32582dcc0be
János Veres <janos@dot.industries>
2024-03-29 12:43:30 +0100

    feat: add access token header to cli requests

--- cli/cmd/client.go
+++ cli/cmd/client.go
@@ -1,10 +1,23 @@
 package cmd
 
 import (
+	"connectrpc.com/connect"
+	"context"
 	"github.com/dotindustries/moar/client"
 	"github.com/dotindustries/moar/moarpb/v1/v1connect"
+	"github.com/labstack/echo/v4"
 )
 
+var clientAuthProviderInterceptor = connect.UnaryInterceptorFunc(func(next connect.UnaryFunc) connect.UnaryFunc {
+	return func(ctx context.Context, request connect.AnyRequest) (connect.AnyResponse, error) {
+		if request.Spec().IsClient && GlobalConfig.AccessToken != "" {
+			// Send a token with client requests.
+			request.Header().Set(echo.HeaderAuthorization, GlobalConfig.AccessToken)
+		}
+		return next(ctx, request)
+	}
+})
+
 func protobufClient() v1connect.ModuleRegistryServiceClient {
-	return client.New(client.Config{Url: GlobalConfig.BackendAddr})
+	return client.New(client.Config{Url: GlobalConfig.BackendAddr}, connect.WithInterceptors(clientAuthProviderInterceptor))
 }
